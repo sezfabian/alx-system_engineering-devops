ac55f98a90941b9ee046758bd4763bf3b6f82f7a
sezfabian <cheruiyotfabian@gmail.com>
2023-04-25 00:48:42 +0300

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp.
 file { 'school':
   path    => '/tmp/school',
-  ensure  => 'file',
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
+  group   => 'www-data'
 }
