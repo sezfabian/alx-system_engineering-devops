cac054b4420b07c1415d6ca223a203b0e88094f3
sezfabian <cheruiyotfabian@gmail.com>
2023-06-12 00:49:31 +0300

    strace webdebug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet automation file to fi WordPress Apache error 500
+exec { 'error500_fix':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# 0x17. Web stack debugging #3
+#   We are Using strace, find out why Apache is returning a 500 error, fixing it and then automate it using Puppet
